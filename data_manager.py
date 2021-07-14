8a7a4e61c55f90e0212deee00f4921e2c5097ea0
alexmoraru97 <alex.moraru97@gmail.com>
2021-07-14 10:14:22 +0300

    add heroku connection files

--- data_manager.py
+++ data_manager.py
@@ -9,6 +9,7 @@ def establish_connection(connection_data=None):
     :connection_data: Connection string attributes
     :returns: psycopg2.connection
     """
+
     if connection_data is None:
         connection_data = get_connection_data()
     try:
@@ -16,13 +17,15 @@ def establish_connection(connection_data=None):
                                                                      connection_data['user'],
                                                                      connection_data['host'],
                                                                      connection_data['password'])
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
+        connection = psycopg2.connect(
+            'postgres://ocgyssxzxltauz:fd5f856a3a45c1c52d9d64ffdd4d7601524d31afbc6fd27eaace7ee1cc160825@ec2-54-228-99-58.eu-west-1.compute.amazonaws.com:5432/d4ei5ig7rtpeiu',
+            sslmode='require')
+        connection.autocommit = True
     except psycopg2.DatabaseError as e:
         print("Cannot connect to database.")
         print(e)
     else:
-        return conn
+        return connection
 
 
 def get_connection_data(db_name=None):
